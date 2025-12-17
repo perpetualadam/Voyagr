08cdf4c19efaf369cf3ca2d8aa98aa8178c35de2
perpetualadam <anamnesisekklesia@googlemail.com>
2025-12-17 19:24:37 +0000

    Update cache-busting version to v=20251217 to force browser refresh

--- voyagr_web.py
+++ voyagr_web.py
@@ -3443,9 +3443,9 @@ HTML_TEMPLATE = '''
     <link rel="stylesheet" href="/static/css/voyagr.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.js"></script>
     <!-- External JavaScript modules -->
-    <script src="/static/js/voyagr-core.js?v=20251216"></script>
-    <script src="/static/js/voyagr-app.js?v=20251216"></script>
-    <script src="/static/js/app.js?v=20251216"></script>
+    <script src="/static/js/voyagr-core.js?v=20251217"></script>
+    <script src="/static/js/voyagr-app.js?v=20251217"></script>
+    <script src="/static/js/app.js?v=20251217"></script>
     <!-- CSS moved to /static/css/voyagr.css -->
 </head>
 <body>
